807be1f4c8e665075a65be8d821ed282d88ee4e8
Siedrix <Siedrix@gmail.com>
2010-12-16 12:10:20 -0600

    started to add tdd to loki ajax, also lots of log functions

--- LokiAjax/lokiAjax.js
+++ LokiAjax/lokiAjax.js
@@ -9,12 +9,41 @@ lokiConsole.debbug({action:'dance'});
 
 (function($){
 	// Our code here...
-	lokiConsole.debbug('Loki Ajax ready to rock');
-
+	$.loki = {
+		console : {
+			_messages : [],
+			log : function(data){
+				this._messages.push(data);
+				if(lokiDebbugStatus.debbug){
+					console.log(data.message);
+				}
+			},
+			getLogByType : function(type){
+				return _(this._messages).chain()
+					.select(function(item){ return item.type == type})
+					.map(function(item){ return item.message})
+					.value();
+			},
+			getLog : function(){
+				return _(this._messages).chain()
+					.map(function(item){ return item.type+':'+item.message})
+					.value();			
+			},
+			flush : function(){
+				this._messages = [];
+				if(lokiDebbugStatus.debbug){
+					console.log('Flushing Console');
+				}				
+			}
+		}
+	}
+	
+	$.loki.console.log({type:'init',message:'Loki ready to rock'});
 	
 	$.lokiAjax = function(options) {
 		ajaxOptions = {};
-
+		
+		lokiConsole.debbug(options.template);
 		if(lokiDebbugStatus.server != 'active'){
 			lokiConsole.debbug('Im doing loki ajax debbug, no server');
 			ajaxOptions.url = options.debbugUrl;
@@ -28,7 +57,40 @@ lokiConsole.debbug({action:'dance'});
 
 		lokiConsole.debbug(ajaxOptions);
 
-		$.ajax(ajaxOptions);
+		if(options.template){
+			ajaxOptions.success = function(data){
+				lokiConsole.debbug('Yei i hava templated success');
+				$.tmpl( options.template, data ).appendTo( options.target );
+			}
+			
+			if($.isTemplateLoaded(options.template)){
+				$.ajax(ajaxOptions);
+			}else{
+				$.loadTemplate(options.template,function(){
+					$.ajax(ajaxOptions)
+				});
+			}
+		}else{
+			$.ajax(ajaxOptions);
+		}
 	}
 
+	$.isTemplateLoaded = function(template){
+		if($.template[template]){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	$.loadTemplate = function(template,callback){
+		$.ajax({
+			'url':'templates/'+template+'.html',
+			'success': function(data){
+				$.template( template, data);
+				//$.tmpl( "myTmpl", {} ).appendTo( "body" );
+				callback(data);
+			}
+		})
+	}
 })(jQuery);
--- LokiAjax/main.js
+++ LokiAjax/main.js
@@ -1,13 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function(){	
+	/*
+	console.log($.isTemplateLoaded('myTemplate'))
+	$.loadTemplate( "myTemplate", function(data){
+	   console.log($.isTemplateLoaded('myTemplate'));
+	   console.log(data);
+	   $.tmpl( "myTemplate", {} ).appendTo( "body" );
+	});
+	*/
+
 	$.lokiAjax({
 		'debbug':false,
 		'data':'loki=true',
 		'type':'GET',
+		'template':'myTemplate',
+		'target':'body',
 		'url':'server/index.php',
-		'debbugUrl': 'json/test.php',
-		'success': function(data){
-			lokiConsole.debbug('Yei i hava success');
-			console.log(data);
-		}
+		'debbugUrl': 'json/test.php'
 	});
 });
